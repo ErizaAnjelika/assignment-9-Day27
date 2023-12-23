b2cbfb92c9573631ede05abc5ba8c6100594457d
ErizAnjelika <erizaangel5@gmail.com>
2023-12-23 20:30:50 +0700

    pindah gambar

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -1,9 +1,9 @@
 import Header from './header';
 
 function Navbar() {
-  const navLogo = ['./public/utilities/img/Logo.png', 'Furniture'];
+  const navLogo = ['/utilities/img/Logo.png', 'Furniture'];
   const navList = ['Home', 'shop', 'About', 'Contact'];
-  const navIcon = ['/utilities/img/icon1.png', '../src/utilities/img/icon2.png', '../src/utilities/img/icon3.png', '../src/utilities/img/icon4.png'];
+  const navIcon = ['/utilities/img/icon1.png', '/utilities/img/icon2.png', '/utilities/img/icon3.png', '/utilities/img/icon4.png'];
   return (
     <div>
       <Header
