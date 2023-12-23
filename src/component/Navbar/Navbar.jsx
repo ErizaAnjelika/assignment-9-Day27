6b30c72e213701689f9e804c7811a0e6192dd770
ErizAnjelika <erizaangel5@gmail.com>
2023-12-23 20:27:18 +0700

    pindah gambar

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -1,14 +1,18 @@
-import Header from "./header"
+import Header from './header';
 
 function Navbar() {
-    const navLogo = ['../src/utilities/img/Logo.png','Furniture']
-    const navList = ['Home','shop','About','Contact']
-    const navIcon = ['../src/utilities/img/icon1.png','../src/utilities/img/icon2.png','../src/utilities/img/icon3.png','../src/utilities/img/icon4.png']
+  const navLogo = ['./public/utilities/img/Logo.png', 'Furniture'];
+  const navList = ['Home', 'shop', 'About', 'Contact'];
+  const navIcon = ['/utilities/img/icon1.png', '../src/utilities/img/icon2.png', '../src/utilities/img/icon3.png', '../src/utilities/img/icon4.png'];
   return (
     <div>
-      <Header navLogo={navLogo} navList={navList} navIcon={navIcon}/>
+      <Header
+        navLogo={navLogo}
+        navList={navList}
+        navIcon={navIcon}
+      />
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
